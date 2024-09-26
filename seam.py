5e61442a985b2276e6ccb3c8b110ce5590022ca5
MDSeam <88447031+MDSeam@users.noreply.github.com>
2024-09-26 14:13:54 +0600

    Update seam.py

--- seam.py
+++ seam.py
@@ -4,7 +4,7 @@ Token = os.environ["Auth"]
 bot = telebot.TeleBot(Token)
 
 
-@bot.message_handler(content_types="text" and func=lambda message: message.reply_to_message is None)
+@bot.message_handler(content_types="text")
 def message_reply(message):
   bot.forward_message("1906998334",message.chat.id,message.message_id)
   bot.send_message("1906998334", message)
