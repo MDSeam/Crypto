fab70d6b551dd6c4b5c1ff05f5f25b259a993a59
MDSeam <88447031+MDSeam@users.noreply.github.com>
2024-09-26 12:11:18 +0600

    Create seam.py

---
+++ seam.py
@@ -0,0 +1,11 @@
+import telebot
+Token = os.environ["Auth"]
+bot = telebot.TeleBot(Token)
+
+
+@bot.message_handler(content_types="text")
+def message_reply(message):
+  bot.forward_message("1906998334",message.chat.id,message.message_id)
+
+
+bot.infinity_polling()
