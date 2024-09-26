1e175b2dc0f655f7db376dc8b795f4a617628522
MDSeam <88447031+MDSeam@users.noreply.github.com>
2024-09-26 14:53:25 +0600

    Update seam.py

--- seam.py
+++ seam.py
@@ -1,17 +1,17 @@
 import telebot
 import os
-from telebot.types import InputFile
+
 Token = os.environ["Auth"]
 bot = telebot.TeleBot(Token)
 
 
 @bot.message_handler(content_types="text")
 def message_reply(message):
-  bot.forward_message("1906998334",message.chat.id,message.message_id)
-  with open("ss.txt","w") as f:
-    f.write(str(message))
-  with open('ss.txt', 'rb') as f:
-    bot.send_document("1906998334",InputFile(f))
+  if message.reply_to_message == None:
+    bot.forward_message("1906998334",message.chat.id,message.message_id)
+  else:
+    bot.send_message(message.reply_to_message.forward_from.id,message.text)
+  
 
 
 
