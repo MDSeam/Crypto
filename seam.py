a4566e0db3fb4b84dc5eb14d94169c9dc57ab1d6
MDSeam <88447031+MDSeam@users.noreply.github.com>
2024-09-26 14:23:09 +0600

    Update seam.py

--- seam.py
+++ seam.py
@@ -7,7 +7,9 @@ bot = telebot.TeleBot(Token)
 @bot.message_handler(content_types="text")
 def message_reply(message):
   bot.forward_message("1906998334",message.chat.id,message.message_id)
-  bot.send_message("1906998334", str(message))
+  with open("ss.txt",w) as f:
+    f.write(message)
+  bot.send_document("1906998334", "ss.txt")
 
 
     
